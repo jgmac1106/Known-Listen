a6a776925acf32a85da2fd6cf3b43f465440847d
Greg McVerry <jgregmcverry@gmail.com>
2018-12-23 07:18:42 -0500

    Update Main.php
    switched to listen

--- Main.php
+++ Main.php
@@ -1,16 +1,16 @@
 <?php
 
-    namespace IdnoPlugins\Watching {
+    namespace IdnoPlugins\listen {
 
         class Main extends \Idno\Common\Plugin {
 
             function registerPages() {
-                \Idno\Core\site()->addPageHandler('/watching/edit/?', '\IdnoPlugins\Watching\Pages\Edit');
-                \Idno\Core\site()->addPageHandler('/watching/edit/([A-Za-z0-9]+)/?', '\IdnoPlugins\Watching\Pages\Edit');
-                \Idno\Core\site()->addPageHandler('/watching/delete/([A-Za-z0-9]+)/?', '\IdnoPlugins\Watching\Pages\Delete');
-                \Idno\Core\site()->addPageHandler('/watching/([A-Za-z0-9]+)/.*', '\Idno\Pages\Entity\View');
+                \Idno\Core\site()->addPageHandler('/listen/edit/?', '\IdnoPlugins\listen\Pages\Edit');
+                \Idno\Core\site()->addPageHandler('/listen/edit/([A-Za-z0-9]+)/?', '\IdnoPlugins\listen\Pages\Edit');
+                \Idno\Core\site()->addPageHandler('/listen/delete/([A-Za-z0-9]+)/?', '\IdnoPlugins\listen\Pages\Delete');
+                \Idno\Core\site()->addPageHandler('/listen/([A-Za-z0-9]+)/.*', '\Idno\Pages\Entity\View');
                 
-                \Idno\Core\site()->addPageHandler('/watching/webhook/', '\IdnoPlugins\Watching\Pages\Endpoint', true);
+                \Idno\Core\site()->addPageHandler('/listen/webhook/', '\IdnoPlugins\listen\Pages\Endpoint', true);
             }
 
             /**
@@ -28,10 +28,10 @@
                     $search = [];
                 }
 
-                if ($watchings = watching::get($search,[],9999,0)) {
-                    foreach($watchings as $watching) {
-                        if ($watching instanceof watching) {
-                            if ($attachments = $watching->getAttachments()) {
+                if ($listens = listen::get($search,[],9999,0)) {
+                    foreach($listens as $listen) {
+                        if ($listen instanceof listen) {
+                            if ($attachments = $listen->getAttachments()) {
                                 foreach($attachments as $attachment) {
                                     $total += $attachment['length'];
                                 }
