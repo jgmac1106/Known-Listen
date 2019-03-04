f8701095027999d5c5fb6f1715a03db412fcabb9
Greg McVerry <jgregmcverry@gmail.com>
2019-03-04 16:35:45 -0500

    Update Listen.tpl.php

--- templates/default/entity/Listen.tpl.php
+++ templates/default/entity/Listen.tpl.php
@@ -12,7 +12,7 @@
 		border-bottom: 1px solid #ccc;
 		}
 	</style>
-	<h2 class="hidden">Listened to <?= htmlentities(strip_tags($vars['object']->getTitle()), ENT_QUOTES, 'UTF-8'); ?></h2>
+	
     <div class="h-cite clearfix">
         <h2>
 	       <?php if (empty($vars['object']->getMediaURL())) { ?>
